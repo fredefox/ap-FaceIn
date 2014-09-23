950e46c7250a62f2a5cd80dcc5c2f1635e32b769
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2014-09-23 13:01:47 +0200

    Add `face_in.pl` with comment-header

---
+++ face_in.pl
@@ -0,0 +1,18 @@
+/*
+* This is an implementation of the "FaceIn"-library.
+*
+* The implementation follows the specification located at:
+*
+*     [advanced programming course
+*     homepage](http://www.diku.dk/~kflarsen/ap-e2014/facein/facein.html)
+*
+* Written by Frederik Hanghøj Iversen
+* for the course Advanced Programming
+* at The University of Copenhagen 2014
+*
+* me@fredefox.eu /^._
+*  ,___,--~~~~--' /'~
+*  `~--~\ )___,)/'
+*      (/\\_  (/\\_
+*
+*/
