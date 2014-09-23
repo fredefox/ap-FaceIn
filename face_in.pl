ac3a1f71911e3215bc04d4135dc609f6b36aca50
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2014-09-23 14:14:38 +0200

    Add implementation of some library functions and `goodfriends`.

--- face_in.pl
+++ face_in.pl
@@ -14,5 +14,67 @@
 *  ,___,--~~~~--' /'~
 *  `~--~\ )___,)/'
 *      (/\\_  (/\\_
-*
 */
+
+/*
+* Utility-functions.
+*/
+member_(A, [A|_]).
+member_(A, [_|AS]) :- member_(A, AS).
+
+is_list_([]).
+is_list_([_|AS]) :- is_list_(AS).
+
+/*
+* This is actually not like `select/3` at all.
+*
+* For instance this is not satisfiable:
+*
+*   select(_, graph([person(_,_)]), _).
+*
+* So, why have I called it `select_`, because I would like to understand why I
+* can't use select like I tried above, maybe I could do it more intellegently.
+* Like make `person`s "selectable". I know that `person` is really just a
+* predicate. Anyways. `select_` is only intended to work with this library.
+*/
+select_(Name, graph([Res|_]), Res) :- Res = person(Name, _).
+select_(Name, graph([_|Persons]), Res) :- select_(Name, graph(Persons), Res).
+/*
+* Basic definitions.
+*/
+
+/*
+* Given:
+*
+*   my_atom(atom(_)).
+*
+* This is false:
+*
+*   my_atom(a).
+*/
+% TODO: `atom` might be disallowed.
+person(Name, List) :-
+	atom(Name),
+	is_list_(List).
+
+graph([]).
+graph([person(_, _)|Persons]) :-
+	graph(Persons).
+
+/*
+* Library-functions
+*/
+
+/*
+* `goodfriends` should take a network as an argument as per specification. It
+* is therefore my thinking that the first two parameters should be the *names*
+* of the people being compared. I will define an ancillary predicate that
+* actually takes two `person`s as arguments.
+*/
+goodfriends_(person(A, AS), person(B, BS)) :-
+	member_(A, BS),
+	member_(B, AS).
+goodfriends(A_name, B_name, Graph) :-
+	select_(A_name, Graph, A),
+	select_(B_name, Graph, B),
+	goodfriends_(A,B).
